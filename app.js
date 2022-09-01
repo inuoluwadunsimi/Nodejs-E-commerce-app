bfa4ce83accbc7e0dfe218fb35ea7fee189d0475
inuoluwadunsimi <danielolaoladeinde@gmail.com>
2022-09-02 00:53:39 +0100

    fixed getCart controller with mongoDb

--- app.js
+++ app.js
@@ -2,12 +2,10 @@ const path = require('path')
 
 const express = require('express')
 const bodyParser = require('body-parser')
-const sequelize = require('./util/database')
-const Product = require('./models/product')
 const User = require('./models/user')
 
-
 const errorController = require('./controllers/error')
+const mongoConnect = require('./util/database').mongoConnect
 
 const app = express()
 
@@ -16,16 +14,14 @@ app.set('views', 'views')
 
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
-const Cart = require('./models/cart')
-const Order = require('./models/order')
-const CartItem = require('./models/cart-item')
-const OrderItem = require('./models/order-item')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
-app.use((req,res,next)=>{
-  User.findByPk(1).then(user=>{
-    req.user = user
+
+
+app.use((req, res, next) => {
+  User.findById('63018b8c1f8e9ebcdad1057d').then(user=>{ 
+    req.user = new User(user.name,user.email,user.cart,user._id) 
     next()
   }).catch(err=>{console.log(err)})
 })
@@ -35,44 +31,7 @@ app.use(shopRoutes)
 
 app.use(errorController.get404)
 
-Product.belongsTo(User, {
-  constraints: true,
-  onDelete: 'CASCADE',
+mongoConnect(() => {
+  
+  app.listen(4000)
 })
-User.hasMany(Product)
-User.hasOne(Cart)
-Cart.belongsTo(User)
-Cart.belongsToMany(Product,{through:CartItem})
-Product.belongsToMany(Cart,{through:CartItem})
-Order.belongsTo(User)
-User.hasMany(Order)
-Order.belongsToMany(Product,{through:OrderItem})
-
-sequelize
-  // .sync({force:true})
-  .sync()
-  .then((result) => {
-    return User.findByPk(1)
-    // console.log(result)
-  })
-  .then((user) => {
-    if (!user) {
-      return User.create({
-        name: 'Lolade',
-        email: 'test@test.com',
-      })
-    }
-    return user
-  })
-  .then((user) => {
-    // console.log(user)
-   return user.createCart()
-  })
-  .then(cart=>{
-    // console.log(cart)
-    app.listen(3000)
-
-  })
-  .catch((err) => {
-    console.log(err)
-  })
--- util/database.js
+++ util/database.js
@@ -1,14 +1,30 @@
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
 
 
-const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize(
-  'node-complete',
-  'root',
-  'Thesaneman12_',
-  {
-    dialect: 'mysql',
-    host: 'localhost',
+let _db
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    'mongodb+srv://Inuoluwadunsimi:Thesaneman12_@cluster0.q9vetjz.mongodb.net/shop?retryWrites=true&w=majority'
+  )
+    .then(client => {
+      console.log('connected!')
+      _db = client.db()
+      callback()
+    })
+    .catch((err) => {
+      console.log(err)
+      throw err
+    })
+
+}
+
+const getDb = () =>{
+  if(_db){
+    return _db
   }
-)
+  throw 'No database found'
+}
 
-module.exports = sequelize
+exports.mongoConnect = mongoConnect
+exports.getDb = getDb
